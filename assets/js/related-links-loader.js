6a63bc57617ef1c53aeba8c01ea7b6f711421d1e
celebrityowned <luxury@celebrityowned.com>
2025-07-20 10:10:05 -0700

    new files

--- assets/js/related-links-loader.js
+++ assets/js/related-links-loader.js
@@ -23,6 +23,61 @@ const relatedCategories = {
     { slug: "funko-pop-collectibles", name: "Funko Pop Collectibles" },
     { slug: "video-games", name: "Video Games" },
     { slug: "disneyana", name: "Disneyana" }
+  ],
+  "magic-the-gathering": [
+    { slug: "pokemon-cards", name: "Pokemon Cards" },
+    { slug: "trading-cards", name: "Trading Cards" },
+    { slug: "graded-pokemon-cards", name: "Graded Pokémon Cards" }
+  ],
+  "yu-gi-oh": [
+    { slug: "trading-cards", name: "Trading Cards" },
+    { slug: "one-piece-tcg", name: "One Piece TCG" },
+    { slug: "magic-the-gathering", name: "Magic: The Gathering" }
+  ],
+  "one-piece-tcg": [
+    { slug: "yu-gi-oh", name: "Yu-Gi-Oh!" },
+    { slug: "trading-cards", name: "Trading Cards" },
+    { slug: "magic-the-gathering", name: "Magic: The Gathering" }
+  ],
+  "disney-tcg": [
+    { slug: "trading-cards", name: "Trading Cards" },
+    { slug: "funko-pop-collectibles", name: "Funko Pop Collectibles" },
+    { slug: "video-games", name: "Video Games" }
+  ],
+  "football-cards": [
+    { slug: "graded-sports-cards", name: "Graded Sports Cards" },
+    { slug: "autographed-sports-cards", name: "Autographed Sports Cards" },
+    { slug: "trading-cards", name: "Trading Cards" }
+  ],
+  "basketball-cards": [
+    { slug: "graded-sports-cards", name: "Graded Sports Cards" },
+    { slug: "autographed-sports-cards", name: "Autographed Sports Cards" },
+    { slug: "trading-cards", name: "Trading Cards" }
+  ],
+  "baseball-cards": [
+    { slug: "graded-sports-cards", name: "Graded Sports Cards" },
+    { slug: "autographed-sports-cards", name: "Autographed Sports Cards" },
+    { slug: "trading-cards", name: "Trading Cards" }
+  ],
+  "ufc-cards": [
+    { slug: "graded-sports-cards", name: "Graded Sports Cards" },
+    { slug: "autographed-sports-cards", name: "Autographed Sports Cards" },
+    { slug: "trading-cards", name: "Trading Cards" }
+  ],
+  "graded-pokemon-cards": [
+    { slug: "pokemon-cards", name: "Pokemon Cards" },
+    { slug: "magic-the-gathering", name: "Magic: The Gathering" },
+    { slug: "graded-sports-cards", name: "Graded Sports Cards" }
+  ],
+  "graded-sports-cards": [
+    { slug: "football-cards", name: "Football Cards" },
+    { slug: "basketball-cards", name: "Basketball Cards" },
+    { slug: "baseball-cards", name: "Baseball Cards" }
+  ],
+  "autographed-sports-cards": [
+    { slug: "graded-sports-cards", name: "Graded Sports Cards" },
+    { slug: "football-cards", name: "Football Cards" },
+    { slug: "basketball-cards", name: "Basketball Cards" }
   ]
 };
 
